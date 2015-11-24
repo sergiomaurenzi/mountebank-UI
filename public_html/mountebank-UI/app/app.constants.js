f5e27829148e747648bfc6443974fc9e9f893e4a
d <dhenton@localhost.com>
2015-11-23 19:49:36 -0600

    fixing readmefixing relative urls 7

--- public_html/mountebank-UI/app/app.constants.js
+++ public_html/mountebank-UI/app/app.constants.js
@@ -1,4 +1,4 @@
 angular.module('app.constants',[])
-         .constant('TPL_PATH', '') //used to prepend a set of folders for templates in app.routes
-         .constant('HEADER_LOCATION',"partials/plainHeader.tpl.html");
+         .constant('TPL_PATH', 'mountebank-UI/') //used to prepend a set of folders for templates in app.routes
+         .constant('HEADER_LOCATION',"mountebank-UI/partials/plainHeader.tpl.html");
  
